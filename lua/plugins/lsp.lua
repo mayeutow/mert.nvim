6638150c3e0307f7cd52ba0acee76a69898deeaa
mayeutow <mayeutow@outlook.com>
2025-05-20 03:33:24 +0300

    sea beyler

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -25,7 +25,7 @@ return {
 
       -- Useful status updates for LSP.
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
+      { 'j-hui/fidget.nvim',       opts = {} },
 
       -- Allows extra capabilities provided by nvim-cmp
       'hrsh7th/cmp-nvim-lsp',
@@ -152,9 +152,6 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        clangd = {},
-        gopls = {},
-        pyright = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -241,7 +238,6 @@ return {
         }
       end,
       formatters_by_ft = {
-        lua = { 'stylua' },
         c = { 'clang-format' },
         go = { 'goimports' },
         html = { 'prettier' },
