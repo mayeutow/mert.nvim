c18e83c907d4ba4c8227e9287dbbc0f54215e62c
mayeutow <mayeutow@outlook.com>
2025-05-20 03:38:28 +0300

    test 2

--- init.lua
+++ init.lua
@@ -6,5 +6,5 @@ require 'themeconfig'
 require 'plugins.health'
 
 -- for some reason this just doesn't work on its own even though it's enabled
--- vim.cmd 'TSEnable highlight'
+vim.cmd 'TSEnable highlight'
 -- vim: ts=2 sts=2 sw=2 et
